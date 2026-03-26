4e24136a3a4a0f717a7ff06459048c7254bbf46c
Dmitrii Sheiko <sheykoda@gmail.com>
2026-03-26 11:27:27 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("application")
+    id("checkstyle")
+    id("org.sonarqube") version "7.1.0.6387"
+    id("jacoco")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "sheykoda-rettani_java-project-72")
+        property("sonar.organization", "sheykoda-rettani")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public final class App {
+    private App() { }
+
+    public static void main(final String[] args) {
+        System.out.println("hello world");
+    }
+}
